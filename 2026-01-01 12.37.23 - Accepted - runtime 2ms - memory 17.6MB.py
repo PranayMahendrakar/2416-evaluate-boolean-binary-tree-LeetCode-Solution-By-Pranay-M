6c5a3ae0954e74caa89f55c462157423af63a5f8
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-13 16:17:17 +0530

    Initial commit

---
+++ 2026-01-01 12.37.23 - Accepted - runtime 2ms - memory 17.6MB.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if root.left is None:  # Leaf node
+            return root.val == 1
+        left = self.evaluateTree(root.left)
+        right = self.evaluateTree(root.right)
+        if root.val == 2:  # OR
+            return left or right
+        else:  # AND (val == 3)
+            return left and right
